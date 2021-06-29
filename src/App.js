aa3c07f3688a1dbc421d289d82941f9ae7701fcc
Marina8989 <marina.mill209@gmail.com>
2021-06-28 19:19:05 -0700

    displayed on screen

--- src/App.js
+++ src/App.js
@@ -4,10 +4,47 @@ import {FaAngleDoubleRight} from 'react-icons/fa';
 const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [jobs, setJobs] = useState([]);
+    const [value, setValue] = useState(0);
+
+    const fetchJob = async () => {
+        const response = await fetch(url);
+        const newJobs = await response.json();
+        setJobs(newJobs);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        fetchJob();
+    }, [])
+
+
+    if(loading) {
+      return (
+          <section className="section loading">
+              <h2>Loading...</h2>
+          </section>
+      )
+    }
+    const {company, title, dates, duties, id} = jobs[value];
     return (
-        <div>
-            <h3>Test...</h3>
-        </div>
+       <section className="section">
+          <h2>Job Experience</h2>
+           <div key={id}>
+                <h3>{title}</h3>
+                <h4>{company}</h4>
+                <p>{dates}</p>
+                {duties.map((duty, index) => {
+                    return(
+                        <div key={index}>
+                            <FaAngleDoubleRight />
+                            <p>{duty}</p>
+                        </div>
+                    )
+                })}
+            </div>
+       </section>
     )
 }
 
